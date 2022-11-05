bae91979894b35a8309e3675fd7265a7e1ffff28
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-05 09:07:43 +0600

    geting products data from db

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try{
+        const productCollection = client.db('emaJohn').collection('products')
+        app.get('/products',async(req,res)=>{
+            const query= {}
+            const cursor=productCollection.find(query)
+            const products= await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally{
 
-
+    }
+}
+run().catch(error=>console.error(error))
 
 
 
